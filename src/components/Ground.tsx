583c01f6f8225f8bcb06e1c1e19a345e76267100
enpitsuLin <lldslm@vip.qq.com>
2021-08-17 00:01:46 +0800

    feat:删除一段无用注视

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -39,24 +39,3 @@ export default class Ground extends Component<Props, { size: string[][] }> {
 		);
 	}
 }
-
-/* export default function Ground(props: Props) {
-	const [size, setSize] = useState([[]]);
-	const { width, height, count } = props;
-	useEffect(() => {
-		setSize(new Array(width).fill(new Array(height).fill('0')));
-	}, []);
-
-	return (
-		<div>
-			{size.map((rowItem, rowIndex) => (
-				<div key={rowIndex}>
-					{rowItem.map((colItem, colIndex) => (
-						<Cell key={colIndex} row={rowIndex} col={colIndex} isMark={false} isOpen={true} isMine={true} adjMine={8} />
-					))}
-				</div>
-			))}
-		</div>
-	);
-}
- */
