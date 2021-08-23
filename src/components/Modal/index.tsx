660eec95b0b0855133f87ea6298da3e3951e72cc
enpitsuLin <lldslm@vip.qq.com>
2021-08-23 19:59:22 +0800

    feat: 简单完成一个modal组件

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './style.scss';
 interface Prop {
-	title: string;
+	title?: string;
 	visible: boolean;
 	onOk: () => void;
 	onCancel: () => void;
@@ -13,20 +13,33 @@ export default class Modal extends Component<Prop> {
 	}
 
 	render() {
+		const { title } = this.props;
+
 		return this.props.visible == true ? (
-			<>
-				<div className="modal-mask" />
-				<div className="modal">
-					<div className="modal__header">
-						<div className="modal__title">{this.props.title}</div>
-						<div className="modal__close" onClick={this.props.onCancel}>
-							X
+			<div>
+				<div className="modal-mask"></div>
+				<div className="modal-wrap" role="dialog">
+					<div className="modal">
+						<div className="modal-content">
+							<button type="button" className="modal-close" aria-label="Close">
+								<span className="modal-close-x">X</span>
+							</button>
+							<div className="modal-header">
+								<div className="modal-title">{title}</div>
+							</div>
+							<div className="modal-body">{this.props.children}</div>
+							<div className="modal-footer">
+								<button className="button">
+									<span>取消</span>
+								</button>
+								<button className="button">
+									<span>确定</span>
+								</button>
+							</div>
 						</div>
 					</div>
-					<div className="modal__content">{this.props.children}</div>
-					<div className="modal__footer">这里是footer</div>
 				</div>
-			</>
+			</div>
 		) : null;
 	}
 }
